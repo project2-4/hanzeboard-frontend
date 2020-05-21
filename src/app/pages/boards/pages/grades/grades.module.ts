3415166e07c5c03f173b5bf23a3fc65195122cbd
Dylan Hiemstra <dylan1337@outlook.com>
2020-05-21 16:49:59 +0200

    Added routing modules

--- src/app/pages/boards/pages/grades/grades.module.ts
+++ src/app/pages/boards/pages/grades/grades.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GradesComponent } from './grades.component';
+import {GradesRoutingModule} from "./grades-routing.module";
 
 @NgModule({
   declarations: [GradesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    GradesRoutingModule
   ]
 })
 export class GradesModule { }
--- src/app/pages/boards/pages/staff/staff.module.ts
+++ src/app/pages/boards/pages/staff/staff.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StaffComponent } from './staff.component';
+import {RouterModule} from "@angular/router";
+import {StaffRoutingModule} from "./staff-routing.module";
 
 @NgModule({
   declarations: [StaffComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    StaffRoutingModule
   ]
 })
 export class StaffModule { }
