c6bba44a70b7397e446f7882e6ad4c3228ef4f68
Dylan Hiemstra <dylan1337@outlook.com>
2020-05-27 13:23:48 +0200

    Added authTokenGrabber.interceptor.ts

--- src/app/shared/interceptors/authTokenGrabber.interceptor.ts
+++ src/app/shared/interceptors/authTokenGrabber.interceptor.ts
@@ -1,28 +1,26 @@
 import {Injectable} from "@angular/core";
-import {HttpInterceptor, HttpRequest, HttpHandler} from "@angular/common/http";
+import {HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse} from "@angular/common/http";
 import {AuthenticationService} from "../services/authentication.service";
+import {map, tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthTokenGrabberInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthenticationService) {
-  }
+  constructor(private authService: AuthenticationService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    // let headers = {};
-    //
-    // if (!req.headers.has('Content-Type')) {
-    //   headers['Content-Type'] = 'application/json';
-    // }
-    //
-    // headers['Authorization'] = `Bearer ${this.authService.getToken()}`;
-    //
-    // // Request is readonly so it needs to be cloned
-    // req = req.clone({
-    //   setHeaders: {Authorization: `Bearer ${this.authService.getToken()}`}
-    // });
-
-    return next.handle(req);
+    return next.handle(req).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          const headers = event.headers;
+          if(headers.keys().includes('authorization')) {
+            // Update token
+            this.authService.accessToken = headers.get('authorization').split('Bearer')[1].trim();
+          }
+        }
+        return event;
+      })
+    );
   }
 }
