8a18aff71f1c2e250e9c4d8051fa75487a81c2cc
Roy Voetman <royvoetman@outlook.com>
2020-05-28 16:17:15 +0200

    Used subscribe on paramMap instead of getter.

--- src/app/pages/boards/pages/courses/pages/subject/subject.component.ts
+++ src/app/pages/boards/pages/courses/pages/subject/subject.component.ts
@@ -10,6 +10,7 @@ import {HttpClient} from '@angular/common/http';
 })
 export class SubjectComponent implements OnInit {
   public course: number;
+  public subject: number;
   public data = [];
 
   constructor(private httpClient: HttpClient, private router: Router, private route: ActivatedRoute) { }
@@ -17,13 +18,17 @@ export class SubjectComponent implements OnInit {
   async ngOnInit() {
     this.course = parseInt(this.route.parent.snapshot.paramMap.get('course'), 10);
 
+    // There’s no need to unsubscribe from the paramMap.
+    // The ActivatedRoute dies with the routed component.
+    this.route.paramMap.subscribe(params => {
+      this.subject = parseInt(params.get('subject'), 10);
+    });
+
     const url = `${environment.apiEndpoint}/courses/${this.course}/subjects/${this.subject}`;
     const request = await this.httpClient.get<any>(url).toPromise();
 
     this.data = request.message;
-  }
 
-  get subject(): number {
-    return parseInt(this.route.snapshot.paramMap.get('subject'), 10);
+    console.log(this.data);
   }
 }
