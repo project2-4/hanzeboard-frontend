bd1d581d542325d97ae06bf357fb5f7ef8a98a55
Roy Voetman <royvoetman@outlook.com>
2020-05-28 13:07:59 +0200

    Fixed Jordi code.

--- src/app/pages/boards/pages/courses/courses-routing.module.ts
+++ src/app/pages/boards/pages/courses/courses-routing.module.ts
@@ -3,7 +3,6 @@ import {NgModule} from '@angular/core';
 import {CourseDetailsComponent} from './pages/course-details/course-details';
 
 const routes: Routes = [
-  { path: '',   redirectTo: '/:id', pathMatch: 'full' },
   {
     path: ':id',
     component: CourseDetailsComponent
