16d960dd8e84872d72d42b69b7d4de1a8279ffb0
Roy Voetman <royvoetman@outlook.com>
2020-06-01 14:22:47 +0200

    Added loader and A2HS

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthenticationService} from '../../services/authentication.service';
 import {Router} from '@angular/router';
@@ -11,20 +11,49 @@ import {Router} from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   form: FormGroup;
+  deferredPrompt: any;
+  showA2HSButton = false;
 
   constructor(private formBuilder: FormBuilder,
               private authService: AuthenticationService,
-              public router: Router) {}
+              public router: Router) {
+  }
 
-  ngOnInit(){
+  ngOnInit() {
     this.form = this.formBuilder.group({
       email: this.formBuilder.control('', Validators.compose(
         [Validators.required, Validators.pattern('^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$')]))
     });
   }
 
-  logout(){
+  logout() {
     this.authService.logout();
   }
 
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    console.log(e);
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showA2HSButton = true;
+  }
+
+  addToHomeScreen() {
+    // hide our user interface that shows our A2HS button
+    this.showA2HSButton = false;
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
+  }
 }
