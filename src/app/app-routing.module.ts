147e7a32dbdaebaecb60357bff52e4e6f46bbf56
Roy Voetman <royvoetman@outlook.com>
2020-06-01 19:34:53 +0200

    Added webp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './pages/login/login.component';
 import {ErrorPageComponent} from './shared/components/error-page/error-page.component';
-import {IsLoggedInGuard} from "./shared/guards/isLoggedIn.guard";
-
-
+import {IsLoggedInGuard} from './shared/guards/isLoggedIn.guard';
 
 const routes: Routes = [
   { path: 'login',
